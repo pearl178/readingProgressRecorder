1bda192d7b3fdb3743be7f54d2fb128a9d5dbf94
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-04-19 23:24:11 -0600

    start on the backend part

---
+++ starting.py
@@ -0,0 +1,6 @@
+# fetch the page information and the "where were you last time" information from the database
+# total_page = 
+# last_time = 
+page_today = int(input("Finished reading today?\nWhich page are you on now?\n"))
+# progress = round((page_today-last_time)/total_page*100)
+# print(f"Great! You've finished {progress}% of the book!")
